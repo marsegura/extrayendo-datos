b42496db3f0d5ec4978ed59451b8dc4a1b33c3d3
JJ Merelo <jjmerelo@gmail.com>
2017-10-23 09:57:24 +0200

    procesando datos y eso

--- scripts/aparcamientos.py
+++ scripts/aparcamientos.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import urllib2
 import csv
+import requests
 
-aparcamientos = urllib2.urlopen('http://datos.madrid.es/egob/catalogo/202625-0-aparcamientos-publicos.csv').read()
-
+aparcamientos = requests.get('http://datos.madrid.es/egob/catalogo/202625-0-aparcamientos-publicos.csv').text
+#with open("../data/202625-0-aparcamientos-publicos.csv","r") as aparcamientos:
 parkingreader= csv.reader(aparcamientos,delimiter=';',quotechar='"',skipinitialspace=True)
 for row in parkingreader:
-    print(', '.join(row))
+    print(row)
+
