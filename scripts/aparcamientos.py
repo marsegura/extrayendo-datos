eed3d61224357927021e75d2cff94d6b2e7e84d8
JJ Merelo <jjmerelo@gmail.com>
2017-10-23 10:47:48 +0200

    Filtra aparcamientos públicos

--- scripts/aparcamientos.py
+++ scripts/aparcamientos.py
@@ -2,11 +2,14 @@
 
 import csv
 import requests
+import re
 
 aparcamientos = requests.get('http://datos.madrid.es/egob/catalogo/202625-0-aparcamientos-publicos.csv').text
 
 #with open("../data/202625-0-aparcamientos-publicos.csv","r") as aparcamientos:
-parkingreader= csv.reader(aparcamientos.split("\n"),delimiter=';',quotechar='"',skipinitialspace=True)
-for row in parkingreader:
-    print(row)
-
+parkingreader= csv.reader(aparcamientos.split("\n"),delimiter=';',quotechar='"')
+#for row in parkingreader:
+#    print(row)
+no_vacios = list( filter( lambda p: p, parkingreader))
+publicos = list( filter( lambda p: re.search(r"público",p[1]), no_vacios))
+print(publicos)
