e74f44e0898289f04b21eb63c389e67f4dd3f2a8
JJ Merelo <jjmerelo@gmail.com>
2017-10-23 10:16:21 +0200

    Solucionado con lista

--- scripts/aparcamientos.py
+++ scripts/aparcamientos.py
@@ -4,8 +4,9 @@ import csv
 import requests
 
 aparcamientos = requests.get('http://datos.madrid.es/egob/catalogo/202625-0-aparcamientos-publicos.csv').text
+
 #with open("../data/202625-0-aparcamientos-publicos.csv","r") as aparcamientos:
-parkingreader= csv.reader(aparcamientos,delimiter=';',quotechar='"',skipinitialspace=True)
+parkingreader= csv.reader(aparcamientos.split("\n"),delimiter=';',quotechar='"',skipinitialspace=True)
 for row in parkingreader:
     print(row)
 
