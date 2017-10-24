e20384b2cc67337827467ac94c37f9b56a4b2581
JJ Merelo <jjmerelo@gmail.com>
2017-10-24 12:37:25 +0200

    Añade aparcamientos en JSON

--- scripts/aparcamientos-dict.py
+++ scripts/aparcamientos-dict.py
@@ -4,7 +4,7 @@ import csv
 import requests
 import re
 
-aparcamientos = requests.get('http://datos.madrid.es/egob/catalogo/202625-0-aparcamientos-publicos.csv').text.split("\n")
+aparcamientos = requests.get('http://datos.madrid.es/portal/site/egob/menuitem.ac61933d6ee3c31cae77ae7784f1a5a0/?vgnextoid=00149033f2201410VgnVCM100000171f5a0aRCRD&format=json&file=0&filename=202625-0-aparcamientos-publicos&mgmtid=26e6cc885fcd3410VgnVCM1000000b205a0aRCRD&preview=full').text
 no_vacios = list( filter( lambda p: len(p)>2, aparcamientos))
 columnas = no_vacios[0].split(";")
 parkingreader= csv.DictReader(no_vacios,fieldnames=columnas,delimiter=';',quotechar='"')
