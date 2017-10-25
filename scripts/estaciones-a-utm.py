a9c73ac2100d0517ef9f3d9979a996273ca58d72
JJ Merelo <jjmerelo@gmail.com>
2017-10-25 13:06:02 +0200

    Parece que ahora sí2

--- scripts/estaciones-a-utm.py
+++ scripts/estaciones-a-utm.py
@@ -15,7 +15,7 @@ with open("../data/estaciones-medioambientales-madrid.csv") as origen:
     for row in estacion_reader:
         cifras_long = re.findall(r"(\d+)",row[3])
         cifras_lat = re.findall(r"(\d+)",row[4])
-        rows.append([row[0],row[1], row[2],decimal(cifras_long),decimal(cifras_lat),row[5]])
+        rows.append([row[0],row[1], row[2],-decimal(cifras_long),decimal(cifras_lat),row[5]])
 
 with open("estaciones-medioambientales-madrid-utm.csv", "w") as csvfile:
     estacion_writer=csv.writer(csvfile,delimiter=";",quotechar='"')
